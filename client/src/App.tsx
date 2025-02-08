8d6f6c0877bdc38493b95f84a70585913b57b9a4
ikramBagban <107988060+IkramBagban@users.noreply.github.com>
2025-02-08 16:22:22 +0530

    design: first page basic design

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,27 @@
 import "./App.css";
 
 function App() {
-  return <div className="bg-[red] w-[40vw]">hello</div>;
+  return (
+    <div className="w-screen h-screen flex justify-center items-center border">
+      <div className=" border space-y-10  ">
+        <div className="font-bold text-lg">Get Stats Of Youtube Video. one click solution</div>
+        <div className="flex flex-col ">
+          <label htmlFor="videoUrl">Enter youtube video url Video URL</label>
+          <div className="space-x-2">
+            <input
+              type="text"
+              name="videoUrl"
+              className="border w-120 p-3 rounded-md"
+              placeholder="Enter youtube video URL"
+            />
+            <button className="w-40 border rounded-md p-3  bg-cyan-500 hover:bg-cyan-400 text-white ">
+              check
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
